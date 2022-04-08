f7526fae58ca654ba8cdbdd4bc591d9b458bff37
RenanQuza <99503650+RenanQuza@users.noreply.github.com>
2022-04-07 23:38:50 -0300

    Delete bmey.atwebpages.com directory

--- bmey.atwebpages.com/listar_save.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "save.php";
-
-$dados = save::ListarTodos();
-$json = json_encode($dados);
-header("Content-Type: application/json; charset=UTF-8");
-echo $json;
--- bmey.atwebpages.com/usuario_teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "usuario.php";
-
-$novoCadastro = new usuario( 1, 'Giovanna');
-$novoCadastro->Cadastrar();
-
-$dados = usuario::ListarTodos();
-var_dump($dados);
